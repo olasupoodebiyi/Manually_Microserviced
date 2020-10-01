e99604ac3b77ec24f7a6cf8fbed1761f1f3355ae
Olasupo <olasupoodebiyi@yahoo.com>
2020-10-01 18:49:17 +0100

    Basics sorted!

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,13 +11,7 @@ const PORT = 4002;
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -40,12 +35,28 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
 
-  console.log(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("Query Service started on PORT " + PORT);
+
+  const { data } = await axios.get("http://localhost:4005/events");
+
+  for (let event of data) {
+    console.log("Processing event: ", event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
